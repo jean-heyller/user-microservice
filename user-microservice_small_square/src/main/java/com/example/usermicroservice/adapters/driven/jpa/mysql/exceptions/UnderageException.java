ff85cfccd135022ca572e73da3854b2cd7c3770a
jean-heyller <heyller-19@outlook.com>
2024-05-20 09:23:08 -0500

    A template is used for the user archetype

---
+++ user-microservice_small_square/src/main/java/com/example/usermicroservice/adapters/driven/jpa/mysql/exceptions/UnderageException.java
@@ -0,0 +1,7 @@
+package com.example.usermicroservice.adapters.driven.jpa.mysql.exceptions;
+
+public class UnderageException extends RuntimeException{
+    public UnderageException() {
+        super();
+    }
+}
---
+++ user-microservice_small_square/src/main/java/com/example/usermicroservice/adapters/driving/http/util/AdapterConstants.java
@@ -0,0 +1,54 @@
+package com.example.usermicroservice.adapters.driving.http.util;
+
+public class AdapterConstants {
+
+    private AdapterConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public enum field{
+        NAME,
+        DESCRIPTION,
+
+    }
+
+    public static final String FIELD_NAME_NULL_MESSAGE = "`name` cannot be null";
+
+    public static final String FIELD_DESCRIPTION_NULL_MESSAGE = "`description` cannot be null";
+
+    public static final String FIELD_DESCRIPTION_SIZE_MESSAGE = "`description` cannot be greater than 90 characters";
+
+    public static final String FIELD_NAME_SIZE_MESSAGE = "`name` cannot be greater than 50 characters";
+
+    public static final String FIELD_LAST_NAME_NULL_MESSAGE = "`last name` cannot be null";
+
+    public static final String FIELD_LAST_NAME_SIZE_MESSAGE = "`last name` cannot be greater than 50 characters";
+
+    public static final String FIELD_IDENTIFICATION_NULL_MESSAGE = "`identification` cannot be null";
+
+    public static final String FIELD_IDENTIFICATION_SIZE_MESSAGE = "`identification` cannot be greater than 50 characters";
+
+    public static final String FIELD_EMAIL_NULL_MESSAGE = "`email` cannot be null";
+
+    public static final String FIELD_EMAIL_SIZE_MESSAGE = "`email` cannot be greater than 50 characters";
+
+    public static final String FIELD_PHONE_NULL_MESSAGE = "`phone` cannot be null";
+
+    public static final String FIELD_EMAIL_VALID_MESSAGE = "`email` should be valid";
+
+    public static final String FIELD_PASSWORD_NULL_MESSAGE = "`password` cannot be null";
+
+    public static final String FIELD_PASSWORD_SIZE_MESSAGE = "`password` cannot be greater than 50 characters";
+
+    public static final String FIELD_ROLE_NULL_MESSAGE = "`role` cannot be null";
+
+    public static final String ROL_AMIN = "ROLE_ADMIN";
+
+    public static final String ROL_TUTOR = "ROLE_TUTOR";
+
+    public static final String FIELD_BIRTH_DATE_NULL_MESSAGE = "`birth date` cannot be null";
+
+
+
+
+}
